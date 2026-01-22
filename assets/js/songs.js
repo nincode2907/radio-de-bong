f1d39be279b68ca35b7ac9528dc869b6d47a7968
nincode2907 <vanninlinhcang@gmail.com>
2026-01-22 08:36:31 +0700

    version 1

---
+++ assets/js/songs.js
@@ -0,0 +1,12 @@
+const playlist = [
+    { id: 1, file: '1.mp3', title: 'Quang Hùng MasterD - Đã Có Anh' },
+    { id: 2, file: '2.mp3', title: 'BẤT CHẤP - WHEE' },
+    { id: 3, file: '3.mp3', title: 'Chúa Tể - Bùi Công Nam' },
+    { id: 4, file: '4.mp3', title: 'EM LÀ AI TRONG ĐÔI MẮT ANH - NGUYỄN TRUNG ĐỨC' },
+    { id: 5, file: '5.mp3', title: 'Lời Yêu buitruonglinh ft. Minsicko' },
+    { id: 6, file: '6.mp3', title: 'Cho em gần anh thêm chút nữa' },
+    { id: 7, file: '7.mp3', title: 'Thôi Đừng Chiêm Bao' },
+    { id: 8, file: '8.mp3', title: 'Người Đầu Tiên - Juky San, Em Xinh Say Hi' },
+    { id: 9, file: '9.mp3', title: 'VSTRA - Ai Ngoài Anh' },
+    // Add more songs here
+];
